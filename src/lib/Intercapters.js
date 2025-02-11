bb07ba9e3e54e39a483a661f8e8a7cce998f9719
Mohit Kushwa <mohit.kushwah@tinyscript.in>
2025-02-11 13:26:22 +0530

    intial deploying

--- src/lib/Intercapters.js
+++ src/lib/Intercapters.js
@@ -6,7 +6,7 @@ import toast from "react-hot-toast";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api"
-    : "http://localhost:5000/api";
+    : "https://notlybackendtest.azurewebsites.net/api";
 
 const axiosInstance = axios.create({
   baseURL,
